9a45b5977e3bdfa65c0830325d5baf451f0037ee
jperry6 <gameranimator101@gmail.com>
2026-02-22 20:33:08 +1100

    catstuff

--- Assets/CatInteract.cs
+++ Assets/CatInteract.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class CatInteract : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class CatInteract : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public Image outlineImage;
     Color startColour;
@@ -21,16 +21,30 @@ public class CatInteract : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (outlineImage != null)
-        {
-            outlineImage.color = new Color(outlineImage.color.r, outlineImage.color.g, outlineImage.color.b, 255);
-        }
+        OutlineCat(true);
     }
     public void OnPointerExit(PointerEventData eventData)
     {
+        OutlineCat(false);
+    }
+    // onpointerclick is used here, somewhere else i will add button presses
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        InteractWithCat();
+    }
+    public void InteractWithCat()
+    {
+
+    }
+
+    void OutlineCat(bool on)
+    {
+        // if the bool is true, set the alpha to full, otherwise set to 0
+        var alpha = on ? 255 : 0;
+
         if (outlineImage != null)
         {
-            outlineImage.color = new Color(outlineImage.color.r, outlineImage.color.g, outlineImage.color.b, 0);
+            outlineImage.color = new Color(outlineImage.color.r, outlineImage.color.g, outlineImage.color.b, alpha);
         }
     }
 }
