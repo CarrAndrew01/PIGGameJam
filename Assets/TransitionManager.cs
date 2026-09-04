aa0fbe30b26a8ba2eac2663dd358f60364aef0b9
JSVortex <gameranimator101@gmail.com>
2026-09-04 20:33:46 +1000

    added keyboard trigger to start transitions for testing

--- Assets/TransitionManager.cs
+++ Assets/TransitionManager.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class TransitionManager : MonoBehaviour
 {
     public static TransitionManager Instance { get; private set;}
+    [SerializeField]
+    Animator topAnimator;
+    [SerializeField]
+    Animator bottomAnimator;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -23,6 +28,16 @@ public class TransitionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.LeftBracket))
+        {
+            topAnimator.SetTrigger("TopClose");
+            bottomAnimator.SetTrigger("BottomClose");
+        } 
+        if (Input.GetKeyDown(KeyCode.RightBracket))
+        {
+            topAnimator.SetTrigger("TopOpen");
+            bottomAnimator.SetTrigger("BottomOpen");
+        }       
     }
+    
 }
