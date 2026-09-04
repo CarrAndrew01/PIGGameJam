1a0b8f1dd5967583205f92916e75b5f785fd5809
JSVortex <gameranimator101@gmail.com>
2026-09-04 20:23:17 +1000

    added events to transition finishes

--- Assets/Scripts/TransitionAnimationEnd.cs
+++ Assets/Scripts/TransitionAnimationEnd.cs
@@ -2,8 +2,22 @@ using UnityEngine;
 
 public class TransitionAnimationEnd : MonoBehaviour
 {
-    public void CompleteAnimation()
+    // This script handles the transition animations starting and finishing
+    // When they finish they fire off these events which allow other things to only happen when transitions are done
+    public void OpenStart()
     {
-        SceneSwitcher.TriggerCompleteTransition();
+          Debug.Log("starting open");
+    }
+    public void OpenFinish()
+    {
+        Debug.Log("finishing open");
+    }
+    public void CloseStart()
+    {
+        Debug.Log("starting close");
+    }
+    public void CloseFinish()
+    {
+        Debug.Log("finishing close");
     }
 }
